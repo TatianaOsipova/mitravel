cd854a1a19523ae2af47534f9a5b8d9e20d2fe2d
Tatiana Osipova <otv120780@gmail.com>
2023-02-03 13:49:19 -0500

    feat: change first slider

--- js/main.js
+++ js/main.js
@@ -59,3 +59,62 @@ playButtonsFirst.forEach((el) => {
 	});
 });
 
+
+
+
+
+// Elements on the page
+const slider = document.querySelector('#slider');
+const sliderItems = Array.from(slider.children);
+const btnNext = document.querySelector('#btnNext');
+const btnPrev = document.querySelector('#btnPrev');
+
+sliderItems.forEach(function (slide, index) {
+    console.log(slide);
+
+    // Hide all the sides except first
+    if (index !== 0) slide.classList.add('hidden');
+
+    // Add index to every slide
+    slide.dataset.index = index;
+
+    // Add data attribute active for first/active slide
+    sliderItems[0].setAttribute('data-active', '');
+
+    // Click on the slides
+    slide.addEventListener('click', function () {
+        showNextSlide('next');
+    });
+});
+
+btnNext.onclick = function() {
+    console.log('Next Slide');
+    showNextSlide('next');
+}
+
+btnPrev.onclick = function() {
+    console.log('Prev Slide');
+    showNextSlide('prev');
+};
+
+function showNextSlide(direction) {
+    // Hide current slide
+    const currentSlide = slider.querySelector('[data-active]');
+    const currentSlideIndex = +currentSlide.dataset.index;
+    currentSlide.classList.add('hidden');
+    currentSlide.removeAttribute('data-active');
+
+    // Calculate the next index depending on the direction
+    let nextSlideIndex;
+    if (direction === 'next') {
+        nextSlideIndex = currentSlideIndex + 1 === sliderItems.length ? 0 : currentSlideIndex + 1;
+    } else if (direction === 'prev') {
+        nextSlideIndex = currentSlideIndex === 0 ? sliderItems.length - 1 : currentSlideIndex - 1;
+    }
+
+     // Show next slide
+    const nextSlide = slider.querySelector(`[data-index="${nextSlideIndex}"]`);
+    nextSlide.classList.remove('hidden');
+    nextSlide.setAttribute('data-active', '');
+}
+
